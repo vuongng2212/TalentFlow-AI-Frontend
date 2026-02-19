b1a523aa8c432142e80d3fce7f41e84299d777ab
Vuong Nguyen <kudoyaung@gmail.com>
2026-02-20 01:03:45 +0700

    feat(candidates): add kanban board components

--- src/components/candidates/index.ts
+++ src/components/candidates/index.ts
@@ -1,3 +1,5 @@
 export { AIScoreBadge } from "./AIScoreBadge";
 export { CandidateCard, CandidateCardOverlay } from "./CandidateCard";
 export { KanbanColumn } from "./KanbanColumn";
+export { KanbanBoard } from "./KanbanBoard";
+export { KanbanSkeleton } from "./KanbanSkeleton";
